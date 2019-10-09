cf6c7d8012c7ca4b25f1ffd4004fcd5ae443da43
aaron atmaca <50340279+erenat77@users.noreply.github.com>
2019-10-09 16:41:50 -0700

    Create test.py
    add the test of BST

---
+++ test.py
@@ -0,0 +1,16 @@
+import BST
+
+# Set up tree
+tree = BST(4)
+
+# Insert elements
+tree.insert(2)
+tree.insert(1)
+tree.insert(3)
+tree.insert(5)
+
+# Check search
+# Should be True
+print tree.search(4)
+# Should be False
+print tree.search(6)
